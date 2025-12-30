33171aec84b097e3258bf40ef3bd5035c71df358
devchalatse <chalatsethabo@gmail.com>
2025-12-30 02:14:41 +0200

    changes

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 from fastapi import FastAPI, Form
 from typing import Annotated
-from model import User
 
 app = FastAPI()
 
-@app.post('/login')
-async def login(new:Annotated[User, Form()]):
-    return new
+@app.post
 
-
-
-
-
+async def root():
+    
