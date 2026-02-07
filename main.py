6f43e015933ab4aae9568f538f963928807f48f2
devchalatse <chalatsethabo@gmail.com>
2026-02-07 18:49:03 +0200

    changes

---
+++ main.py
@@ -0,0 +1,25 @@
+class Calculator():
+    def add_one(self,x):
+        return x + 1
+    
+    def subtraction(self, x):
+        return x - 1
+    
+    def multiplication(self, x):
+        return x * 1
+    
+    def divide(self, x):
+        return x % 1
+    
+num = Calculator()
+# num.add_one()
+num.subtraction()
+num.divide()
+
+print(num.add(1))
+print(num.subtraction(2))
+print(num.divide(3))
+
+
+
+
