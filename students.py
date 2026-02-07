e2f566061be464a60d67758ecada5506abea8031
devchalatse <chalatsethabo@gmail.com>
2026-02-08 00:07:06 +0200

    changes in get_grades function

--- students.py
+++ students.py
@@ -1,4 +1,4 @@
-class Students():
+class Student():
     def __init__(self, name, age, grades):
         self.name = name
         self.age = age
@@ -18,18 +18,21 @@ class Course():
             self.students.append(student)
             return True
         return False
+    
     def average_student(self):
-        value = 0
-        for student in self.students:
-            value += Students.get_grades(self) 
-        return value/len(Students)
+        if not self.students:
+            return 0
         
-s1 = Students('Thabo',32, 85)
-s2 = Students('Chalatse', 19, 90)
+        total = sum(student.get_grades() for student in self.students)
+        return total / len(self.students)
 
-course1 = Course('CompScience', 1)
-(course1.add_student(s1))
-(course1.add_student(s2))
-print(course1.average_student())
+s1 = Student('Thabo', 32, 85)
+s2 = Student('Chalatse', 19, 90)
 
+course1 = Course('CompScience', 2)
+
+course1.add_student(s1)
+course1.add_student(s2)
+
+print(course1.average_student())  
 
