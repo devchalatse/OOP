6dbf03acb26edb64a98c7b18ffb28294ce9d86e6
devchalatse <chalatsethabo@gmail.com>
2026-02-07 23:04:04 +0200

    The methods

---
+++ students.py
@@ -0,0 +1,35 @@
+class Students():
+    def __init__(self, name, age, grades):
+        self.name = name
+        self.age = age
+        self.grades = grades
+        
+    def get_grades(self):
+        return self.grades
+    
+class Course():
+    def __init__(self, name, max_students):
+        self.name = name
+        self.max_students = max_students
+        self.students = []
+
+    def add_student(self, student):
+        if len(self.students) < self.max_students:
+            self.students.append(student)
+            return True
+        return False
+    def average_student(self):
+        value = 0
+        for student in self.students:
+            value += Students.get_grades(self) 
+        return value/len(Students)
+        
+s1 = Students('Thabo',32, 85)
+s2 = Students('Chalatse', 19, 90)
+
+course1 = Course('CompScience', 1)
+(course1.add_student(s1))
+(course1.add_student(s2))
+print(course1.average_student())
+
+
